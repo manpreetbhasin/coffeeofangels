0aa64ad5777a5e83211f3d7c321672941c7ecd29
Manpreet Bhasin <manpreetbhasin@gmail.com>
2024-10-18 10:39:05 -0700

    Adding object instead of img tag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <object type="image/svg+xml" data={logo}  className="App-logo" alt="logo" ></object>
         <p className='text'>
           "Brewing one cup at time"
         </p>
